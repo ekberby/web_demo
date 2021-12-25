83ef280fc7c58131ca9487df7a732eb1c3d81d77
Valid Akhundov <valid.akhundov@pashabank.az>
2021-12-25 12:45:21 +0400

    minor fix

--- Web_demo0/src/test/java/com/example/web_demo0/service/UserDetailServiceTest.groovy
+++ Web_demo0/src/test/java/com/example/web_demo0/service/UserDetailServiceTest.groovy
@@ -9,21 +9,24 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import spock.lang.Specification
 
-class UserDetailServiceTest extends Specification{
+class UserDetailServiceTest extends Specification {
 
     private final UserRepository userRepository = Mock()
     private final UserDetailsService userDetailsService = new UserDetailServiceImpl(userRepository)
 
-    def "test loadUserByUsername"(){
+    def "test loadUserByUsername"() {
         def id = "a"
-        UserDetails userDetails = new UserPrincipal(User.builder().username("a").password("a").firstName("a")
-                .lastName("a").email("a").role(Role.LANDLORD).build())
-        def optional = Optional.of(User.builder().username("a").password("a").firstName("a")
-                .lastName("a").email("a").role(Role.LANDLORD).build())
+        UserPrincipal userPrincipal = new UserPrincipal(User.builder().username("a").password("a")
+                .role(Role.LANDLORD).build())
+        def optional = Optional.of(User.builder().username("a").password("a").role(Role.LANDLORD).build())
+
         when:
         def result = userDetailsService.loadUserByUsername(id)
+
         then:
-        1*userRepository.findById(id) >> optional
-        result == userDetails
+        1 * userRepository.findById(id) >> optional
+        result.getUsername() == userPrincipal.getUsername()
+        result.getPassword() == userPrincipal.getPassword()
+        result.getAuthorities() == userPrincipal.getAuthorities()
     }
 }
